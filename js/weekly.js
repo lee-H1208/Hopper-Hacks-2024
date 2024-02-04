b07fe96feb94bba30c74fac393e6b89ee9b7e10e
Xinyue <xysu05@gmail.com>
2024-02-04 11:44:14 -0500

    moving pancake

---
+++ js/weekly.js
@@ -0,0 +1,37 @@
+/* const div = document.querySelector(".menu-items");
+let offsetX, offsetY;
+
+const move = (e) => {
+    div.style.left = '$(e.clientX - offsetX)px';
+    div.style.top = '$(e.clientY - offsetY)px';
+}
+div.addEventListener("mousedown",(e) =>{
+    offsetX = e.clientX - div.offsetLeft;
+    offsetY = e.clientY - div.offsetTop;
+    document.addEventListener("mousemove", move);
+});
+
+document.addEventListener("mouseup",()=>{
+    document.removeEventListener("mousemove", move);
+});*/
+
+var drag;
+function dragMove(id){
+    var element = document.getElementById("pancake");
+    element.style.position="absolute";
+    element.onmousedown=function(){
+        drag=element;
+    }
+}
+
+document.onmouseup=function(e){
+    drag=null;
+}
+
+document.onmousemove=function(e){
+    var x = e.pageX;
+    var y = e.pageY;
+
+    drag.style.left = x + "px";
+    drag.style.top = y + "px";
+}
