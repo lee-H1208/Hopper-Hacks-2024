3e8f09828cda9585a004ee9eff172370285670bb
lee-H1208 <leeh120824@gmail.com>
2024-02-04 11:14:33 -0500

    added schedule function

--- js/input.js
+++ js/input.js
@@ -10,3 +10,16 @@ function tdAdd() {
         listTD.appendChild(li);
     }
 }
+
+const inputSchBox = document.getElementById("inputSch");
+const listSch = document.getElementById("listSch-container");
+
+function schAdd() {
+    if(inputSchBox.value === '') {
+        alert("You must write something.");
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputSchBox.value;
+        listSch.appendChild(li);
+    }
+}
